c16caf3e5e9c9be2ff9e0856c752dd2206e6806a
Philipp Altmann <altmannp@cip.ifi.lmu.de>
2017-03-06 15:06:29 +0100

    Added Setup function to set Workstation Positions

--- FactorySimulator.py
+++ FactorySimulator.py
@@ -4,8 +4,6 @@ from Product import Product
 from Workstation import Workstation
 
 class FactorySimulator:
-    products = []
-    workStations = {}
 
     currentFieldStatus = [
         [0, 0, 0],
@@ -14,34 +12,56 @@ class FactorySimulator:
     ]
 
     def generate_products(self, path_to_json):
+        products = []
         with open(path_to_json) as data_file:
             data = json.load(data_file)
             for item in data['products']:
-                self.products.append(Product(item['positionX'],item['positionY']))
+                products.append(Product(item['positionX'], item['positionY']))
+        return products
 
     def generate_work_stations(self, path_to_json):
+        workStations = {}
         with open(path_to_json) as data_file:
             data = json.load(data_file)
             for item in data['workStations']:
-                if not item['type'] in self.workStations:
+                if not item['type'] in workStations:
                     # Add new Workstation type
-                    self.workStations.update({item['type']: []})
+                    workStations.update({item['type']: []})
 
                 # Add New Workstation to List of existing type
-                self.workStations[item['type']].append(Workstation(item['type']))
+                workStations[item['type']].append(Workstation(item['type']))
+        return workStations
 
     def __init__(self, path_to_products_json, path_to_workstations_json):
-        self.generate_products(path_to_products_json)
-        self.generate_work_stations(path_to_workstations_json)
+        self.products = self.generate_products(path_to_products_json)
+        self.workStations = self.generate_work_stations(path_to_workstations_json)
         pprint(self.products)
         pprint(self.workStations)
-        pprint(self.workStations.values())
 
-    #def setup(self, position_list):
+    def count_workstations(self):
+        count = 0
+        for item in self.workStations.values():
+            count += len(item)
+        return count
+
+    def set_position_for_workstations(self, workstation_positions):
+        # Check if length is matchig
+        if self.count_workstations() == len(workstation_positions):
+            # iterate over input
+            for item in workstation_positions:
+                # Pop, setPosition, append Workstation Objects to maintain order
+                workstation = self.workStations[item[0]].pop(0)
+                workstation.setPosition(item[1], item[2])
+                self.workStations[item[0]].append(workstation)
         # for workstations ... setPosition
+        else:
+            raise Exception("Too many positions in workstation_positions")
+
+    def setup(self, workstation_positions):
+        self.set_position_for_workstations(workstation_positions)
 
 
 Factory = FactorySimulator('Products.json', 'Workstations.json')
-position_list = []
-#Factory.setup()
+position_list = [('A', 10, 20), ('B', 5, 10), ('C', 20, 10), ('A', 20, 20),  ('D', 5, 5)]
+Factory.setup(position_list)
 
