deca341fd36caade25dad541fbbf6f56f2f53bfb
Peter Schnitzler <Torlek@gmail.com>
2017-03-08 11:30:18 +0100

    does not work

--- Factory.py
+++ Factory.py
@@ -9,22 +9,26 @@ import time
 import sys
 from enum import Enum
 
+
+ARRAYSIZE = 11
+
 class visibitltyStatus(Enum):
     NONE = 0
     WorkstationPos = 1
     ALL = 2
 
 
+class FactoryGenerator:
 
-class FactorySimulator:
-    ARRAYSIZE = 11
-    view = None
+    def __init__(self, path_to_products_json, path_to_workstations_json):
+        self.workStations = {}
+        with open(path_to_workstations_json) as jsonFile:
+            self.json = json.load(jsonFile)
+        self.products = self.generate_products(path_to_products_json, self.workStations)
+        self.currentFieldStatus = self.initFieldStatus()
+        self.counter = 0
 
-    def initFieldStatus(self):
-        r = []
-        for i in range(0, self.ARRAYSIZE):
-            r.append([False] * self.ARRAYSIZE)
-        return r
+
 
     def generate_products(self, path_to_json, workStations):
         """ Generates products from the specified JSON file """
@@ -35,33 +39,12 @@ class FactorySimulator:
                 products.append(Product(item['positionX'], item['positionY'], item["workstationRoute"], workStations))
         return products
 
-    """def generate_work_stations(self, path_to_json):
-        #Generates workstations from the specified JSON file
-        workStations = {}
-        with open(path_to_json) as data_file:
-            data = json.load(data_file)
-            for item in data['workStations']:
-                if not item['type'] in workStations:
-                    # Add new Workstation type
-                    workStations.update({item['type']: []})
+   def initFieldStatus(self):
+        r = []
+        for i in range(0, self.ARRAYSIZE):
+            r.append([False] * self.ARRAYSIZE)
+        return r
 
-                # Add New Workstation to List of existing type
-                workStations[item['type']].append(Workstation(item['type']))
-        return workStations
-    """
-    def __init__(self, path_to_products_json, path_to_workstations_json, vs):
-        self.workStations = {}
-        with open(path_to_workstations_json) as jsonFile:
-            self.json = jsonFile.load(jsonFile)
-        self.products = self.generate_products(path_to_products_json, self.workStations)
-        self.currentFieldStatus = self.initFieldStatus()
-        self.counter = 0
-        self.vs = vs
-        if vs != visibitltyStatus.NONE:
-            self.viewRoot = Tk()
-
-        """pprint(self.products)"""
-        """pprint(self.workStations)"""
 
     def count_workstations(self):
         """ Counts the workstations saved in FactorySimulator """
@@ -69,6 +52,7 @@ class FactorySimulator:
         for item in self.workStations.values():
             count += len(item)
         return count
+
 
     def set_position_for_workstations(self, workstation_positions):
         """ Update workstation positions with Tupel (Type, x, y) - typically from evolutionary algorithm """
@@ -81,25 +65,44 @@ class FactorySimulator:
             # Create new Worstation object
             ws = Workstation(item[0], item[1], item[2])
             # Add time at ws constraint to ws
-            ws.setTimeAtWs(self.json[item[0]]['timeAtWs'])
+            for typeDef in self.json['workStations']:
+                if typeDef['type'] == item[0]:
+                    ws.setTimeAtWs(typeDef['time_at_ws'])
+
             self.workStations[item[0]].append(ws)
+
 
     def setup(self, workstation_positions):
         """ Sets up the factory """
         self.set_position_for_workstations(workstation_positions)
-        if self.vs != visibitltyStatus.NONE:
-            self.View = View(self.viewRoot, self.products, self.workStations)
-            self.viewRoot.geometry("1000x600+300+50")
         self.productReset()
 
-    def checkWorkstationConstrait(self):
+
+    def checkWorksta_to_ptionConstrait(self):
         # Check number of ws types
         if not len(self.json['workStations']) == len(self.workStations):
             raise Exception("Workstation constrait violated")
         for item in self.json['workStations']:
             # Check count of ws for every type
             if not len(self.workStations[item['type']]) == item['count']:
-                 raise Exception("Workstation constrait violated")
+                raise Exception("Workstation constrait violated")
+
+
+class Factory:
+
+    def __init__(self, ws, p, cfs, c, vs):
+        self.workStations = ws
+        self.products = p
+        self.currentFieldStatus = cfs
+        self.counter = c
+        self.vs = vs
+        self.View = None
+        if vs != visibitltyStatus.NONE:
+            self.viewRoot = Tk()
+            self.View = View(self.viewRoot, self.products, self.workStations)
+            self.viewRoot.geometry("1000x600+300+50")
+
+
 
 
     def productReset(self):
@@ -148,7 +151,7 @@ class FactorySimulator:
         return returnVal
 
 viz_type = visibitltyStatus.NONE
-Factory = FactorySimulator('Products.json', 'Workstations.json', viz_type)
+facGen = FactoryGenerator('Products.json', 'Workstations.json', viz_type)
 position_list = [('A', 3, 10), ('B', 2, 9), ('C', 7, 0), ('A', 6, 6), ('D', 1, 5)]
 
 Factory.setup(position_list)
